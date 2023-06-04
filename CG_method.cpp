dbe35892b294961059b2f5c07cfb826e1e10e652
KuoJuiWu <125047432+KuoJuiWu@users.noreply.github.com>
2023-06-04 18:31:38 +0800

    CG_parallel_test02
    I am trying to update my script.

--- CG_method.cpp
+++ CG_method.cpp
@@ -4,6 +4,115 @@
 #include <time.h>
 #include "def.h"
 
+/*
+try push
+#include <cstdlib>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+*/
+/*
+Variable used for iteration or intermediate results within the loops can be declared as private
+Variable used to accumulate or aggregate results across iterations or threads need to be declared as shared
+
+In our CG method: r = A*x-B
+Shared: A(matrix), B(matrix), resid, and x
+private: i,j (the element of each array, like A[i], B[j])
+
+parallel should make at least three part:
+1. summing up each part innner product of vector
+2. vector and matrix multiply
+3. calculating each x
+Maybe A and B should also use parallel?
+
+We choose reduction except of atomic is because of the size of the array. If the total number of elements are small, 
+atomic method is more convenient.
+trying to update myscript
+*/
+/*
+double dotproduct(double* A, double * B, int N) {
+ double sum = 0.0;
+ #pragma omp parallel for reduction(+ : sum)
+  for (int i = 0; i < N; i++)
+  {
+   sum += A[i] * B[i];
+   //pragma omp barrier: it is not necessary because reduction already have the function of sychronization.
+  }
+  return sum;
+}
+double* vectorAdd(double* A, double* B, int N) {
+ double* temp = (double*)calloc(N, sizeof(double));
+ #pragma omp parallel shared(temp, A, B) private(i){
+  for (int i = 0; i < N; i++) {
+   temp[i] = A[i] + B[i];
+  }
+  return temp;
+  free(temp);
+ }
+}
+double* vectorSubtract(double* A, double* B, int N) {
+ double* temp = (double*)calloc(N, sizeof(double));
+ int i;
+ #pragma omp parallel shared(temp, A, B) private(i){
+ for (int i = 0; i < N; i++) {
+  temp[i] = A[i] - B[i];
+ }
+ return temp;
+ free(temp);
+ }
+}
+
+double* vectorZoom(double* A, double scaler, int N) {
+ int i;
+ #pragma omp parallel shared(A) private(i){
+  for (int i = 0; i < N; i++){
+   A[i] = scaler * A[i];
+  }
+  return A;
+ }
+
+}
+
+double* matrix_Times_vector(double* M, double* V, int N) {
+ double* temp = (double*)calloc(N, sizeof(double));
+ int i,j;
+ #pragma omp parallel for collapse(2) shared(temp, M,V) private(i,j) reduction(+:temp){
+  for (int i = 0; i < N; i++)
+  for (int j = 0; j < N; j++) {
+    temp[i] += M[i * N + j] * V[j];
+    // pragma omp barrier
+  }
+  return temp;
+  free(temp);
+ }
+}
+double start = omp_get_wtime();
+//work//
+double end = omp_get_wtime();
+printf("Work took %f seconds \n", end-start);
+*/
+/*
+int main{
+ #pragma omp parallel shared(A,B) private(i,j){}
+ double a[6] = { 0 };
+ double b[6] = { 0 };
+
+
+ for (int i = 0; i < 6; i++) {
+  a[i] = double(i);
+  b[i] = double(i);
+ }
+
+ 
+ for (int i = 0; i < 6; i++) {
+  printf("%f\n",c[i]);
+ }
+ //printf("hello world");
+ 
+ return EXIT_SUCCESS;
+}
+*/
+
 
 void CG_method(double *x0, double tol){ // tol: tolerance
 	int i, j, k;
